d779e563c5922c4f2c6e7d83acb6053cbfaa4067
craig <craig@DESKTOP-BJL3O96>
2021-10-25 14:32:09 +1100

    Working on ball collision

---
+++ level ideas/Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(4, 0, 4), ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
