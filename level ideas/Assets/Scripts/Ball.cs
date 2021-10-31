840704efcee60a487a756a9a173c3eb16b275db9
Jasper Eyers <jaspereyers@hotmail.com>
2021-11-01 10:15:21 +1100

    Feature (ball) : 2d/3d modes
    changed ball to work with 2d and 3d modes while also cleaning some unused code

--- level ideas/Assets/Scripts/Ball.cs
+++ level ideas/Assets/Scripts/Ball.cs
@@ -6,10 +6,22 @@ public class Ball : MonoBehaviour
 {
     public float speed;
     public Rigidbody rb;
+    [SerializeField] GameManager GM;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(new Vector3(4, 0, 4), ForceMode.Impulse);
+        if (GM.is3d)
+        {
+            rb.AddForce(new Vector3(4, 0, 4), ForceMode.Impulse);
+            rb.constraints = RigidbodyConstraints.None;
+        }
+        else
+        {
+            rb.AddForce(new Vector3(0, 0, 4), ForceMode.Impulse);
+            rb.constraints = RigidbodyConstraints.FreezePositionX;
+        }
+
     }
 }
--- level ideas/Assets/Scripts/BallBounce.cs
+++ level ideas/Assets/Scripts/BallBounce.cs
@@ -4,40 +4,8 @@ using UnityEngine;
 
 public class BallBounce : MonoBehaviour
 {
-    //[SerializeField] bool X;
-    //[SerializeField] bool Y;
-    //[SerializeField] bool Z;
     public GameManager GM;
-    //
-    //private Vector3 initialVelocity;
-    //
-    //[SerializeField]
-    //private float minVelocity = 10f;
-    //
-    //private Vector3 lastFrameVelocity;
-    //
-    //[SerializeField] Rigidbody rb;
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    Debug.Log("HIT");
-    //    if (other.gameObject.tag == "Ball")
-    //    {            
-    //        Debug.Log("HIT");
-    //        if (X)
-    //        {
-    //            rb.velocity = new Vector3(-rb.velocity.x * Random.value, rb.velocity.y, rb.velocity.z) * GM.acceleration;
-    //        }
-    //        else if (Y)
-    //        {
-    //            rb.velocity = new Vector3(rb.velocity.x, -rb.velocity.y * Random.value, rb.velocity.z) * GM.acceleration;
-    //        }
-    //        else if (Z)
-    //        {
-    //            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y * Random.value, -rb.velocity.z) * GM.acceleration;
-    //        }
-    //    }
-    //}
     [SerializeField]
     [Tooltip("Just for debugging, adds some velocity during OnEnable")]
     private Vector3 initialVelocity;
