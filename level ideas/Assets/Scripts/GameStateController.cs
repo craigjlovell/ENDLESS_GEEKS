85e3af945fbff373f221e6b0297abc2ab31c8081
Craig Lovell <craigjlovell1999@gmail.com>
2021-11-01 11:09:09 +1100

    Working on gamestates

---
+++ level ideas/Assets/Scripts/GameStateController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateController : MonoBehaviour
+{
+    public Canvas canvas = null;
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("box");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void GamePause()
+    {
+        Time.timeScale = 0;
+        canvas = GetComponentInParent<Canvas>();
+        canvas.enabled = false;
+        canvas = GameObject.FindGameObjectWithTag("").GetComponent<Canvas>();
+        canvas.enabled = true;
+    }
+    public void GameResume()
+    {
+        Time.timeScale = 1;
+        canvas = GetComponentInParent<Canvas>();
+        canvas.enabled = false;
+        canvas = GameObject.FindGameObjectWithTag("").GetComponent<Canvas>();
+        canvas.enabled = true;
+    }
+}
