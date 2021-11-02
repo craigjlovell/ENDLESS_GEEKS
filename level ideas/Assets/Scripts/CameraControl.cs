e9d737b44116a502420939dc58572a5ca451d656
Jasper Eyers <jaspereyers@hotmail.com>
2021-11-02 12:20:51 +1100

    Cameratweaks branch pseudo merge

--- level ideas/Assets/Scripts/CameraControl.cs
+++ level ideas/Assets/Scripts/CameraControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public GameObject camObj;
+    public GameObject CamMTObj;
     private Camera cam;
     public int camPos;
     private Vector3 newPos;
@@ -12,6 +13,7 @@ public class CameraControl : MonoBehaviour
     [SerializeField] float smooth;
     public Quaternion target;
     [SerializeField] Score score;
+    private bool hit = false;
 
     private void Start()
     {
@@ -23,56 +25,53 @@ public class CameraControl : MonoBehaviour
         camPos = score.scoreCam;
         if (camPos == -4)
         {
-            newPos = new Vector3(0, 0, -20);
             target = Quaternion.Euler(0, 0, 0);
         }
         else if (camPos == -3)
         {
-            newPos = new Vector3(10, 0, -19);
             target = Quaternion.Euler(0, -22.5f, 0);
         }
         else if (camPos == -2)
         {
-            newPos = new Vector3(13, 0, -15);
             target = Quaternion.Euler(0, -45, 0);
         }
         else if (camPos == -1)
         {
-            newPos = new Vector3(17, 0, -8);
             target = Quaternion.Euler(0, -67.5f, 0);
         }
         else if (camPos == 0)
         {
-            newPos = new Vector3(15, 0, 0);
             target = Quaternion.Euler(0, -90, 0);
         }
         else if (camPos == 1)
         {
-            newPos = new Vector3(17, 0, 8);
             target = Quaternion.Euler(0, -112.5f, 0);
         }
         else if (camPos == 2)
         {
-            newPos = new Vector3(13, 0, 15);
             target = Quaternion.Euler(0, -135, 0);
         }
         else if (camPos == 3)
         {
-            newPos = new Vector3(10, 0, 19);
             target = Quaternion.Euler(0, -157.5f, 0);
         }
         else if (camPos == 4)
         {
-            newPos = new Vector3(0, 0, 20);
             target = Quaternion.Euler(0, -180, 0);
         }
-
-        if (score.scorePlayer1 != 0 || score.scorePlayer2 != 0)
+        if (!hit)
         {
-            camObj.transform.position = Vector3.Lerp(camObj.transform.position, newPos, camSpeed);
-            cam.fieldOfView = 90;
-            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+            if (score.scorePlayer1 != 0 || score.scorePlayer2 != 0)
+            {
+                newPos = new Vector3(15, 0, 0);
+                camObj.transform.position = Vector3.Lerp(camObj.transform.position, newPos, camSpeed);
+                cam.fieldOfView = 90;
+                hit = true;
+                Debug.Log("zoom");
+            }
         }
+        CamMTObj.transform.rotation = Quaternion.Slerp(CamMTObj.transform.rotation, target, Time.deltaTime * smooth);
+        Debug.Log("rotate");
     }
 }
 
