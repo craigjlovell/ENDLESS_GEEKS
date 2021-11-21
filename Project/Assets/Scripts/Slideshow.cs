bcda317245d8d82ff99246c63736b36c404d944a
Jasper <s210228@students.aie.edu.au>
2021-11-22 09:16:06 +1100

    fixed customisation

--- Project/Assets/Scripts/Slideshow.cs
+++ Project/Assets/Scripts/Slideshow.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class Slideshow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int slide;
+    [SerializeField] int numberOfSlides;
+    [SerializeField] string objectName;
+
+    [SerializeField] GameObject object4;
+    [SerializeField] GameObject object2;
+    [SerializeField] GameObject object3;
+    [SerializeField] GameObject object1;
+
+    private void Start()
     {
-        
+        slide = PlayerPrefs.GetInt(objectName, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Next()
     {
-        
+        if (slide == numberOfSlides - 1)
+        {
+            slide = 0;
+        }
+        else
+        {
+            slide++;
+        }
+    }
+
+    void Previous()
+    {
+        if (slide == 0)
+        {
+            slide = numberOfSlides - 1;
+        }
+        else
+        {
+            slide++;
+        }
+    }
+
+    private void Update()
+    {
+        if (slide == 0)
+        {
+            object1.SetActive(true);
+        }
+        else
+        {
+            object1.SetActive(false);
+        }
+        if (slide == 1)
+        {
+            object2.SetActive(true);
+        }
+        else
+        {
+            object2.SetActive(false);
+        }
+        if (slide == 2)
+        {
+            object3.SetActive(true);
+        }
+        else
+        {
+            object3.SetActive(false);
+        }
+        if (slide == 3)
+        {
+            object4.SetActive(true);
+        }
+        else
+        {
+            object4.SetActive(false);
+        }
+        PlayerPrefs.SetInt(objectName, slide);
     }
 }
